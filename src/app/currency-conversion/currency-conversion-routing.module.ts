4c5e7b7d9c5d889d804a2a518783861358a827b2
Shashi Kiran <kiranshashi14@outlook.com>
2021-03-05 00:51:29 +0530

    Updates to publish library

--- src/app/currency-conversion/currency-conversion-routing.module.ts
+++ src/app/currency-conversion/currency-conversion-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CurrencyConversionComponent, CurrencyConversionModule } from 'currency-conversion';
+import { CurrencyConversionComponent, CurrencyConversionModule } from 'currency-conversion-lib';
 
 const routes: Routes = [{ path: '', component: CurrencyConversionComponent }];
 
