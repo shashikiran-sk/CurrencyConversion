7a53d53563fb6228263a73f95c78f323087ebbd2
Shashi Kiran <kiranshashi14@outlook.com>
2021-03-04 21:58:30 +0530

    Update reducer as injection token

--- projects/currency-conversion-lib/src/app/currency-conversion.module.ts
+++ projects/currency-conversion-lib/src/app/currency-conversion.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CurrencyConversionComponent } from './currency-conversion.component';
 import { CurrencyInputComponent } from './components/currency-input/currency-input.component';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducerMap, StoreModule } from '@ngrx/store';
 import { reducers, currencyConversionStateKey } from './reducers/index';
 import { EffectsModule } from '@ngrx/effects';
 import { CurrencyEffects } from './effects/currencyEffects';
@@ -11,16 +11,18 @@ import { FormsModule } from '@angular/forms';
 import { CurrencyService } from './services/currency.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+const REDUCERS_TOKEN = new InjectionToken<ActionReducerMap<typeof reducers>>('CurrencyConversion Reducers');
+
 @NgModule({
     declarations: [CurrencyConversionComponent, CurrencyInputComponent],
     imports: [
         CommonModule,
         FormsModule,
         NgbModule,
-        StoreModule.forFeature(currencyConversionStateKey, reducers),
+        StoreModule.forFeature(currencyConversionStateKey, REDUCERS_TOKEN),
         EffectsModule.forFeature([CurrencyEffects])
     ],
-    providers: [CurrencyService],
+    providers: [CurrencyService, { provide: REDUCERS_TOKEN, useValue: reducers }],
     exports: [CurrencyConversionComponent]
 })
 export class CurrencyConversionModule {}
